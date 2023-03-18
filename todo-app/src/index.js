71de307d4a10f967de3fa14e10fe5fa270b79dd1
Thibault-De-Permentier <thibault.DE-PERMENTIER@etu.univ-amu.fr>
2023-03-18 13:21:02 +0100

    1er version

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -1,17 +1,61 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import  ReactDOM from 'react-dom/client';
+import { Checkbox } from "antd";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class ListTask extends React.Component{
+    render(){
+        let listTasks = [];
+        
+        for (let i = 0; i < this.props.tasks.length; i++) {
+           listTasks.push(<div className="task"> 
+           <Checkbox checked={this.props.tasks[i]['isChecked']}></Checkbox>
+           <p>{this.props.tasks[i]['title']}</p>
+           <div className="movingArrow">
+               <button onClick={null}></button>
+               <button onClick={null}></button>
+           </div>
+       </div>)
+        }
+        return(
+            <div id='list'>
+                {listTasks}
+            </div>
+        )
+    }
+}
+
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            tasks: JSON.parse(localStorage.getItem('tasks'))
+        };
+    }
+    addTask(){
+        tasks.push({"title":document.getElementById('newTask').nodeValue, "isChecked":false});
+    }
+
+    render(){
+        return(
+            <div>
+                <header>
+
+                </header>
+                    <ListTask tasks={this.state.tasks}/>
+                <footer>
+                    <input id='newTask' type={'text'}/>
+                    <button onClick={this.addTask}>+</button>
+                </footer>
+            </div>    
+        )
+    }
+}
+
+//==================================================================================================
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
+var tasks = [{"title":"1.Idée","isChecked":true},{"title":"2.Marché","isChecked":true},{"title":"3.Wireframe","isChecked":true},{"title":"4.Design","isChecked":true},{"title":"5.Landingpage","isChecked":true},{"title":"6.Développement","isChecked":false},{"title":"7.Publish","isChecked":false},{"title":"8.Pub","isChecked":false},{"title":"9.Feedback","isChecked":false}]
+localStorage.setItem('tasks',JSON.stringify(tasks))
