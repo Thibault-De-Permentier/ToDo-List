53b5d643e0feedd95195bd0d4c2ef7ae275b9289
Thibault-De-Permentier <thibault.DE-PERMENTIER@etu.univ-amu.fr>
2023-03-20 09:48:01 +0100

    les boutons fonctionnes

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -4,16 +4,28 @@ import { Checkbox } from "antd";
 
 
 class ListTask extends React.Component{
+    moveUpTask(i){
+        if(i == 0)
+            return;
+        this.props.tasks[i] = [this.props.tasks[i-1], this.props.tasks[i-1] = this.props.tasks[i]][0];
+        this.forceUpdate();        
+    }
+    moveDownTask(i){
+        if(i == this.props.tasks.length-1)
+            return;
+        this.props.tasks[i] = [this.props.tasks[i+1], this.props.tasks[i+1] = this.props.tasks[i]][0];
+        this.forceUpdate();        
+    }
+
     render(){
         let listTasks = [];
-        
         for (let i = 0; i < this.props.tasks.length; i++) {
            listTasks.push(<div className="task"> 
            <Checkbox checked={this.props.tasks[i]['isChecked']}></Checkbox>
            <p>{this.props.tasks[i]['title']}</p>
            <div className="movingArrow">
-               <button onClick={null}></button>
-               <button onClick={null}></button>
+               <button onClick={() => this.moveUpTask(i)}>UP</button>
+               <button onClick={() => this.moveDownTask(i)}>DOWN</button>
            </div>
        </div>)
         }
@@ -32,9 +44,13 @@ class App extends React.Component{
         this.state = {
             tasks: JSON.parse(localStorage.getItem('tasks'))
         };
+        this.addTask = this.addTask.bind(this);
     }
     addTask(){
-        tasks.push({"title":document.getElementById('newTask').nodeValue, "isChecked":false});
+        let temp = this.state.tasks;
+        let tempTitle = this.state.tasks.length + 1 + '.' + document.getElementById('newTask').value;
+        temp.push({"title":tempTitle, "isChecked":false});
+        this.setState({tasks:temp});
     }
 
     render(){
@@ -57,5 +73,5 @@ class App extends React.Component{
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
 
-var tasks = [{"title":"1.Idée","isChecked":true},{"title":"2.Marché","isChecked":true},{"title":"3.Wireframe","isChecked":true},{"title":"4.Design","isChecked":true},{"title":"5.Landingpage","isChecked":true},{"title":"6.Développement","isChecked":false},{"title":"7.Publish","isChecked":false},{"title":"8.Pub","isChecked":false},{"title":"9.Feedback","isChecked":false}]
-localStorage.setItem('tasks',JSON.stringify(tasks))
+/*var tasks = [{"title":"1.Idée","isChecked":true},{"title":"2.Marché","isChecked":true},{"title":"3.Wireframe","isChecked":true},{"title":"4.Design","isChecked":true},{"title":"5.Landingpage","isChecked":true},{"title":"6.Développement","isChecked":false},{"title":"7.Publish","isChecked":false},{"title":"8.Pub","isChecked":false},{"title":"9.Feedback","isChecked":false}]
+localStorage.setItem('tasks',JSON.stringify(tasks))*/
